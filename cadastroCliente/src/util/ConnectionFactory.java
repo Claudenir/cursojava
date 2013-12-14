278938f50e0112a03b88133f779e88880a8a9794
lab02 <lab02@01-LAB2-03.ACADEMICO.LOCAL>
2013-12-14 18:07:06 -0200

    Desenvolvimento

---
+++ cadastroCliente/src/util/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	private static Connection con;
+	
+	public static Connection getConnection(){
+		
+		try{
+			con = DriverManager.getConnection("jbdc:postgres://192.168.0.108","postgres","123");
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
